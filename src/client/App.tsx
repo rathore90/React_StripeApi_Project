1efdddcb7bba38bbce1d5614cc8cd44accbc461d
rathore90 <rathore90@hotmail.com>
2021-02-20 00:38:44 -0800

    wip

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,63 +1,23 @@
 import * as React from 'react';
+import {StripeProvider, Elements} from 'react-stripe-elements';
+import Form from './Form';
 
-class App extends React.Component<IAppProps, IAppState> {
-	constructor(props: IAppProps) {
-		super(props);
-		this.state = {
-			name: null
-		};
-	}
 
-	// async componentDidMount() {
-	// 	try {
-	// 		let r = await fetch('/api/hello');
-	// 		let name = await r.json();
-	// 		this.setState({ name });
-	// 	} catch (error) {
-	// 		console.log(error);
-	// 	}
-	// }
+export default class App extends React.Component<IAppProps, IAppState> {
 
 	render() {
 		return (
-			<main className="container my-5">
-				<h1 className="text-primary text-center">Hello {this.state.name}!</h1>
-			</main>
+			<>
+			<StripeProvider apiKey="pk_test_51HAQ8GHDkmXqGeZTqGsVbqI9Q3ulKvWNqiiLkceJO5cXHyulHyyk5A2VWbDX4dJDptC7248jaFrGGuXpz1Jr7W5P007aUoMX4O">
+				<Elements>
+					<Form />
+				</Elements>
+			</StripeProvider>
+			</>
 		);
 	}
 }
 
 export interface IAppProps {}
 
-export interface IAppState {
-	name: string;
-}
-
-export default App;
-
-//
-// const App = (props: AppProps) => {
-// 	const [greeting, setGreeting] = React.useState<string>('');
-
-// 	React.useEffect(() => {
-// 		(async () => {
-// 			try {
-// 				const res = await fetch('/api/hello');
-// 				const greeting = await res.json();
-// 				setGreeting(greeting);
-// 			} catch (error) {
-// 				console.log(error);
-// 			}
-// 		})();
-// 	}, []);
-
-// 	return (
-// 		<div className="min-vh-100 d-flex justify-content-center align-items-center">
-// 			<h1 className="display-1">Hello {greeting}!</h1>
-// 		</div>
-// 	);
-// };
-
-// interface AppProps {}
-
-// export default App;
+export interface IAppState {}
---
+++ src/client/Form.tsx
@@ -0,0 +1,48 @@
+import * as React from 'react';
+import { CardElement, injectStripe, ReactStripeElements} from 'react-stripe-elements';
+
+class Form extends React.Component<IFormProps, IFormState>{
+    
+    constructor(props: IFormProps){
+        super(props);
+        this.state = {
+            name: ""
+            amount: ""
+        };
+    }
+
+    render() {
+        return(
+            <main className="container">
+                <form action="" className="form-group mt-3 border border-primary rounded shadow-lg p-3">
+                    <label>
+                        Name
+                    </label>
+                    <input 
+                        type="text"
+                        className="input-group my-1 p-1 border border-dark"
+                        value={this.state.name}
+                        onChange={(e: React.ChangeEvent<HTMLInputElement>) => this.setState({ name: e.EventTarget.value})}
+                    />
+                    <label>Amount</label>
+                    <input 
+                        type="text"
+                        className="input-group my-1 p-1 border border-dark"
+                        value={this.state.name}
+                        onChange={(e: React.ChangeEvent<HTMLInputElement>) => this.setState({ name: e.EventTarget.value})}
+                    />
+                    <CardElement className="">
+                </form>
+            </main>
+        );
+    }
+}
+
+
+interface IFormProps { }
+
+interface IFormState { 
+    name: string;
+}
+
+export default injectStripe(Form);
